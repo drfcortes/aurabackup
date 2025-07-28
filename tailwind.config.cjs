8fabe779bb8e801acf56b66267cb6ecca18c7935
Fausto David Cortés Rojas <fdavid.cortezr@cinvestav.mx>
2025-07-28 14:41:41 -0600

    Fix: tailwind config restored, build successful

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
-    ],
+    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
     theme: {
         extend: {},
     },
     plugins: [],
-}
+};
