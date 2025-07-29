da73c452e86e98a5eadd54b2bea3e947cd2472c0
Fausto David Cortés Rojas <fdavid.cortezr@cinvestav.mx>
2025-07-29 15:06:14 -0600

    ampliacion

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,11 +9,32 @@ module.exports = {
         extend: {
             colors: {
                 aura: {
-                    green: '#636b3f',       // Marine Green
-                    deep: '#2b361c',        // Deep Green
-                    yellow: '#fefae3',      // Barium Yellow
-                    sepia: '#d4a369',       // Sepia
-                    leather: '#b17036',     // Leather
+                    fontSize: {
+                        fontFamily: {
+
+                            // Paleta original
+                            green: '#636b3f',
+                            deep: '#2b361c',
+                            yellow: '#fefae3',
+                            sepia: '#d4a369',
+                            leather: '#b17036',
+
+                            // Variantes nuevas
+                            soft: '#f9f9f6',           // Fondo casi blanco
+                            beige: '#eceadf',          // Alternativa neutral clara
+                            black: '#1a1a1a',          // Negro moderno (para headers si lo deseas)
+                            gray: '#5e5e5e',           // Gris intermedio
+                            white: '#ffffff',          // Blanco puro
+                            olive: '#8b8f66',          // Verde oliva (complementario)
+                            cream: '#fbf9ee',          // Alternativa muy suave de fondo
+
+
+                            base: '1.125rem', // equivale a 18px en lugar de 16px
+                            lg: '1.25rem',
+
+                            sans: ['Noto Sans', 'sans-serif'],
+                        }
+                    }
                 }
             }
         }
