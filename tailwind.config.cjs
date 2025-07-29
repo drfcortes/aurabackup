942c9cee5d19ce38aa86fe76c960ace508609c3b
Fausto David Cortés Rojas <fdavid.cortezr@cinvestav.mx>
2025-07-29 13:28:41 -0600

    Fix: corrceting colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
-    },
-    plugins: [],
+        extend: {
+            fontSize: {
+                base: '18px', // ← sube de 16px (por defecto) a 18px
+            },
+            colors: {
+                auraWine: '#4B2E39',
+            }
+        },
+        plugins: [],
+    }
 };
