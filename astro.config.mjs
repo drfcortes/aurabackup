86a94d450469b70e7d55a10179bc1c7b4f55048a
Fausto David Cortés Rojas <fdavid.cortezr@cinvestav.mx>
2025-08-04 12:45:08 -0600

    Migrando a versel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless'; // 👈 Nuevo
 
 export default defineConfig({
     site: 'https://aurablock.org',
-    output: 'server',
-    adapter: node({ mode: 'standalone' }),
+    output: 'server', // SSR
+    adapter: vercel(), // 👈 Usar adaptador de Vercel
     integrations: [react(), tailwind(), sitemap()],
 });
